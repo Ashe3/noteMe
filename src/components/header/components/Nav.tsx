01e538ef0ff73f156b0ddc316aeb0816b763bdfc
Anton Shevchenko <korolewsky.anton3@gmail.com>
2022-11-22 22:29:31 +0200

    add/nav and logo

---
+++ src/components/header/components/Nav.tsx
@@ -0,0 +1,16 @@
+import { navStyles } from './styles';
+
+const Nav = () => (
+  <nav css={navStyles}>
+    <ul>
+      <li>
+        <a href="#">Notes</a>
+      </li>
+      <li>
+        <a href="#">Else</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
